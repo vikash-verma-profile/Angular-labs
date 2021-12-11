945aad4e228e3d27530f4bc66f23614192b98c23
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-12-11 15:38:41 +0530

    first commit

--- Labs/Typescript-labs/lab-5.ts
+++ Labs/Typescript-labs/lab-5.ts
@@ -4,16 +4,36 @@
 
 class Shape{
     Area:Number;
-    constructor(_area:number) {
+    constructor(_area:Number) {
         this.Area=_area;
     }
 }
 class Circle extends Shape{
+    radius:Number;
+    constructor(_radius:Number,_area:Number) {
+        super(_area);
+        this.radius=_radius;
+    }
+
     Display():void
     {
         console.log("Area of the circle"+this.Area);
     }
 }
 
-var obj=new Circle(223);
+var obj=new Circle(2,223);
 obj.Display();
+
+//Multi level
+class Root{
+    sample:string;
+}
+class Child extends Root{
+
+}
+class Leaf extends Child{
+
+}
+var objL=new Leaf();
+objL.sample="5";
+console.log(objL.sample);
