855e182f4312aa9149adb46f1cf58f7e7546c4fe
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-12-18 14:31:04 +0530

    events-application

--- Labs/events-application/events-app/src/app/events/event.component.ts
+++ Labs/events-application/events-app/src/app/events/event.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { EventService } from '../services/event.service';
+import { Event } from './event.model';
 
 @Component({
   selector: 'app-events',
   templateUrl: './event.eventview.html',
 })
 export class EventsComponent {
-  title = 'events-app';
+    events:Array<Event>=new Array<Event>();
+  constructor(private _eventService:EventService) {}
+  ngOnInit(){
+    this._eventService.getEvents().subscribe(res=>this.events=res,err=>console.log(err));
+    console.log(this.events);
+}
 }
--- Labs/events-application/events-app/src/app/services/event.service.ts
+++ Labs/events-application/events-app/src/app/services/event.service.ts
@@ -3,12 +3,14 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class EventService{
-    private _eventUrl="";
-    private _specialEventsUrl="";
+    private _eventUrl="http://localhost:3000/api/events";
+    private _specialEventsUrl="http://localhost:3000/api/special";
 
     constructor(private http:HttpClient){}
 
     getEvents(){
+        //this.http.get<any>(this._eventUrl).subscribe(res=>console.log(res),err=>console.log(err))
+
         return this.http.get<any>(this._eventUrl);
     }
 
